05dd8027cb0e5522715bb711fc43c03c70a13293
Shota Yamamota <1230124fw@gmail.com>
2017-03-10 00:04:26 +0900

    add sen

--- main.py
+++ main.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html', message="Hello")
 
+@app.route("/app/<appname>",methods=["GET"])
+def test(appname):
+    return render_template('sen.html',app=appname)
+
 if __name__ == "__main__":
     app.run()
