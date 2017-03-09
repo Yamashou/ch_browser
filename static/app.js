f937cbb45bdeef08473c240262371fe6d6c0875c
Shota Yamamota <1230124fw@gmail.com>
2017-03-09 22:06:46 +0900

    test

---
+++ static/app.js
@@ -0,0 +1,24 @@
+$(function() {
+  //ユーザエージェント
+  var userAgent = window.navigator.userAgent;
+  //バージョン
+  var appVersion = window.navigator.appVersion;
+  //名前
+  var appName = window.navigator.appName;
+  //プラットフォームのタイプ
+  var platform = window.navigator.platform;
+  //コード名
+  var appCodeName = window.navigator.appCodeName;
+  console.log(appVersion);
+  $("#box2").text(userAgent);
+  $("#box3").text(appVersion);
+  $("#box4").text(appName);
+  $("#box5").text(platform);
+  $("#box6").text(appCodeName);
+  if(userAgent.match(/Line/)){
+    $("#ch-id").text("LINE");
+  }else
+  if(userAgent.match(/FBAV/)){
+    $("#ch-id").text("Facebook");
+  }
+});
