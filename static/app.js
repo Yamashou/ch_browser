926b4e71c2c088c09ec387c9331812fa51503a38
Yamashou <1230124fw@gmail.com>
2017-03-10 11:28:36 +0900

    test sss

--- static/app.js
+++ static/app.js
@@ -9,7 +9,7 @@ $(function() {
   var platform = window.navigator.platform;
   //コード名
   var appCodeName = window.navigator.appCodeName;
-  console.log(appVersion);
+  console.log(jQuery.browser.msie);
   $("#box2").text(userAgent);
   $("#box3").text(appVersion);
   $("#box4").text(appName);
