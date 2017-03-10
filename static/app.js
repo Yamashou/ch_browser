773a330028019c374e25ddb66b10bbdf7b6b869a
Yamashou <1230124fw@gmail.com>
2017-03-10 11:46:54 +0900

    ddd

--- static/app.js
+++ static/app.js
@@ -9,7 +9,7 @@ $(function() {
   var platform = window.navigator.platform;
   //コード名
   var appCodeName = window.navigator.appCodeName;
-  console.log(jQuery.browser.msie);
+  console.log(jQuery.support);
   $("#box2").text(userAgent);
   $("#box3").text(appVersion);
   $("#box4").text(appName);
