112fea71054f7141277ab395e7913498bdf19436
Yamashou <1230124fw@gmail.com>
2017-03-10 15:26:21 +0900

    rm comment

--- static/test2.js
+++ static/test2.js
@@ -1,10 +1,8 @@
 $(function(){
   const userAgent = navigator.userAgent.toLowerCase();
   if (userAgent.search(/iphone|ipad|ipod/) > -1) {
-    // Launch myapp via URL scheme
     launch_frame.location.href= "Prefs:root=SAFARI&path=https://aqueous-fjord-46391.herokuapp.com";
     setTimeout(function(){
-      // Open App DL page in iTunes Store
       location.href= "https://www.kayac.com/";
     } , 500);
   }else if(userAgent.search(/android/) > -1){
