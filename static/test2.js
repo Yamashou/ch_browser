f80b030904a23cd53e1807cc2e0edb260f3f5638
Yamashou <1230124fw@gmail.com>
2017-03-10 15:25:51 +0900

    test

--- static/test2.js
+++ static/test2.js
@@ -1,16 +1,14 @@
 $(function(){
-  var userAgent = navigator.userAgent.toLowerCase();
+  const userAgent = navigator.userAgent.toLowerCase();
   if (userAgent.search(/iphone|ipad|ipod/) > -1) {
     // Launch myapp via URL scheme
-    launch_frame.location.href= "{{ app }}://";
+    launch_frame.location.href= "Prefs:root=SAFARI&path=https://aqueous-fjord-46391.herokuapp.com";
     setTimeout(function(){
       // Open App DL page in iTunes Store
       location.href= "https://www.kayac.com/";
     } , 500);
   }else if(userAgent.search(/android/) > -1){
     setTimeout(function(){
-      // Open App DL page in iTunes Store　http://d.hatena.ne.jp/
-      // location.href= "android-app://com.android.chrome/https/aqueous-fjord-46391.herokuapp.com/app/test";
       location.href="intent://aqueous-fjord-46391.herokuapp.com#Intent;scheme=https;action=android.intent.action.VIEW;package=com.android.chrome;end";
     } , 500);
   }
