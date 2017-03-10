85d7a287d7393c73beb25d922dab7aa69ff8201f
Yamashou <1230124fw@gmail.com>
2017-03-10 09:52:20 +0900

    ch page

--- static/app.js
+++ static/app.js
@@ -9,6 +9,18 @@ $(function() {
   var platform = window.navigator.platform;
   //コード名
   var appCodeName = window.navigator.appCodeName;
+  var iframe = document.createElement('iframe');
+  iframe.style.border = 'none';
+  iframe.style.width = '1px';
+  iframe.style.height = '1px';
+
+  iframe.src = 'twitter://post?message=' + encodeURIComponent('ほげほげ #ふがふが');
+  document.body.appendChild(iframe);
+
+  iframe.onload = function() {
+    window.location.href = 'market://details?id=com.twitter.android';
+    iframe.parentNode.removeChild(iframe);
+  };
   console.log(appVersion);
   $("#box2").text(userAgent);
   $("#box3").text(appVersion);
