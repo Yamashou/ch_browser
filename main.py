206c09d22c2bc9fcf1247479f6290af880e30961
Yamashou <1230124fw@gmail.com>
2017-03-13 11:46:56 +0900

    test

--- main.py
+++ main.py
@@ -5,9 +5,6 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route("/app/<appname>",methods=["GET"])
-def test(appname):
-    return render_template('sen.html',app=appname)
 
 if __name__ == "__main__":
     app.run()
